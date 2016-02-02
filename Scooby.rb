edf358f11231f662125e7936fa8daa523e1c62d9
nhaines81 <neil.haines@hotmail.com>
2016-02-02 16:14:24 -0500

    initial commit

---
+++ Scooby.rb
@@ -0,0 +1,8 @@
+pets = ["Scooby", "Soco", "Summer", "Pixie", "Wilson", "Mason","Baron", "Brinkley", "Bella"]
+pets.each do |i|
+    if i.start_with?("S")
+        puts "My name starts with an S for super!"
+    else
+        puts "I’m still pretty special too!"
+    end
+end
